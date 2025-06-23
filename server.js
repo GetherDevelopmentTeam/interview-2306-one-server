388a8d96ccd41247e9f833d814cf25b3d1d9581c
rohit0033 <rohusharma2003@gmail.com>
2025-06-23 18:11:37 +0530

    cors error

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/databse");
-const corsMiddleware = require("./middleware/cors");
+// const corsMiddleware = require("./middleware/cors");
 const app = express();
 
 const port = 5702;
 
 dotenv.config();
 connectDB();
-app.use(corsMiddleware);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
